d2061814de7c08aefddc098c2e56aefd9a9ed32d
Goldenboard <380volt@naver.com>
2021-11-01 19:44:44 +0900

    강의 1: 구성 가능한 함수

--- week 1-Jetpack Compose basics/Codelab_1/app/src/main/java/com/example/codelab_1/MainActivity.kt
+++ week 1-Jetpack Compose basics/Codelab_1/app/src/main/java/com/example/codelab_1/MainActivity.kt
@@ -14,25 +14,18 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Codelab_1Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            MessageCard("Android")
             }
         }
-    }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(name: String) {
+    Text(text = "Hello $name")
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    Codelab_1Theme {
-        Greeting("Android")
-    }
+fun PreviewMessageCard() {
+    MessageCard("Android")
 }
