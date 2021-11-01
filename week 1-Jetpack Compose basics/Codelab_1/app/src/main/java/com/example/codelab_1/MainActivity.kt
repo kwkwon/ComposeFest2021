cb053987bc6e2f5fc841dc31da153d7a72f76ab6
Goldenboard <380volt@naver.com>
2021-11-01 20:05:26 +0900

    강의 2: 레이아웃

--- week 1-Jetpack Compose basics/Codelab_1/app/src/main/java/com/example/codelab_1/MainActivity.kt
+++ week 1-Jetpack Compose basics/Codelab_1/app/src/main/java/com/example/codelab_1/MainActivity.kt
@@ -3,10 +3,14 @@ package com.example.codelab_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.codelab_1.ui.theme.Codelab_1Theme
 
@@ -14,18 +18,32 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard("Android")
+            MessageCard(Message("Android", "Jeckpack Compose"))
             }
         }
 }
 
+data class Message(val author: String, val body: String)
+
 @Composable
-fun MessageCard(name: String) {
-    Text(text = "Hello $name")
+fun MessageCard(msg: Message) {
+    Row() {
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "Contact profile picture")
+
+        Column() {
+            Text(text = msg.author)
+            Text(text = msg.body)
+        }
+    }
 }
 
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard("Android")
+    MessageCard(
+        msg = Message("Colleague", "Hey, ....")
+    )
 }
