ba9fe5f7fc51ad63ac4b246a4d566d9b77845b34
Goldenboard <380volt@naver.com>
2021-11-04 12:25:47 +0900

    greeting

--- week 1-Jetpack Compose basics/codelab1/app/src/main/java/com/example/codelab1/MainActivity.kt
+++ week 1-Jetpack Compose basics/codelab1/app/src/main/java/com/example/codelab1/MainActivity.kt
@@ -3,11 +3,16 @@ package com.example.codelab1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.codelab1.ui.theme.Codelab1Theme
 
 class MainActivity : ComponentActivity() {
@@ -16,20 +21,30 @@ class MainActivity : ComponentActivity() {
         setContent {
             Codelab1Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+private fun MyApp() {
+    androidx.compose.material.Surface(color = MaterialTheme.colors.background) {
+        Greeting(name = "Android")
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun Greeting(name: String) {
+    Surface(color = MaterialTheme.colors.primary) {
+        Column(modifier = Modifier.padding(24.dp)) {
+            Text(text = "Hello")
+            Text(text = "$name!")
+        }
+    }
+}
+
+@Preview(showBackground = true, name = "Text Preview")
 @Composable
 fun DefaultPreview() {
     Codelab1Theme {
