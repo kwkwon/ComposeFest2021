9cb9134016c4e48b557440ebabd9a16e7f771f83
Goldenboard <380volt@naver.com>
2021-11-22 17:17:41 +0900

    Testing begins

---
+++ week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -0,0 +1,10 @@
+package com.example.compose.rally
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import org.junit.Rule
+
+class TopAppBarTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+}
